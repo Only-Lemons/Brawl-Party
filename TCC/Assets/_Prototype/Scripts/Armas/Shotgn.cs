67b8ae337f4b8301902f4173f408d12fc712c543
ppsavila <ppsavila2@gmail.com>
2019-08-27 15:03:06 -0300

    teste pra armas

---
+++ TCC/Assets/_Prototype/Scripts/Armas/Shotgn.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgn : Arma
+{
+    public override void Shoot()
+    {
+        throw new System.NotImplementedException();
+    }
+}
