6f496dfd4e1cdc377a5aeda645c3aeee6bc9ccc8
wevertonmbw <wevertonmbw@gmail.com>
2019-09-06 16:46:44 -0300

    Update CameraController2.cs

--- TCC/Assets/_Prototype/Scripts/Camera/CameraController2.cs
+++ TCC/Assets/_Prototype/Scripts/Camera/CameraController2.cs
@@ -8,17 +8,15 @@ public class CameraController2 : MonoBehaviour
 {
     // Start is called before the first frame update
     Camera cam;
-   
+
     public List<GameObject> targ = new List<GameObject>();
     void Start()
     {
         cam = Camera.main;
-        getTargets();
-
-
+        GetTargets();
 
     }
-    void getTargets()
+    void GetTargets()
     {
         targ.Clear();
         GameObject[] aux = GameObject.FindGameObjectsWithTag("Player");
@@ -28,34 +26,26 @@ public class CameraController2 : MonoBehaviour
         }
     }
 
-   
+
     void LateUpdate()
     {
-        
-        FollowZoom();
+        ControleFoco();
 
-       
     }
 
-    void FollowZoom()
+    void ControleFoco()
     {
         cam.transform.LookAt(MediaDistancia());
+        CameraMan();
 
-        if (Vector3.Distance(targ[0].transform.position, targ[1].transform.position) > 20)
-        {
-            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, 90, Time.deltaTime * 5);
-        }
-        if (Vector3.Distance(targ[0].transform.position, targ[1].transform.position) <= 20 && Vector3.Distance(targ[0].transform.position, targ[1].transform.position) >= 10)
-        {
-            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, 60, Time.deltaTime * 5);
-        }
-
-        if ((Vector3.Distance(targ[0].transform.position, targ[1].transform.position) < 10))
-        {
-            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, 30, Time.deltaTime * 5);
-        }
+        cam.fieldOfView = ZoomDistancia(targ[0].transform, targ[1].transform, targ[2].transform, targ[3].transform);
+        //cam.fieldOfView = ZoomDistancia(targ);
     }
 
+    void CameraMan()
+    {
+
+    }
     Vector3 MediaDistancia()
     {
         Vector3 m = Vector3.zero;
@@ -65,6 +55,36 @@ public class CameraController2 : MonoBehaviour
         }
 
         return m / targ.Count;
+    }
+
+    float ZoomDistancia(Transform a, Transform b, Transform c, Transform d)
+    {
+        float z = 0;
+        if (a != null || b != null || c != null || d != null)
+        {
+            if (Vector3.Distance(a.position, b.position) > 20 || Vector3.Distance(a.position, c.position) > 20 || Vector3.Distance(a.position, d.position) < 10 || Vector3.Distance(b.position, c.position) < 10 || Vector3.Distance(b.position, d.position) > 20 || Vector3.Distance(c.position, d.position) > 20)
+            {
+                z = Mathf.Lerp(cam.fieldOfView, 90, Time.deltaTime * 5);
+            }
+            if (Vector3.Distance(a.position, b.position) <= 20 || Vector3.Distance(a.position, c.position) <= 20 || Vector3.Distance(a.position, d.position) <= 20 || Vector3.Distance(b.position, c.position) < 10 || Vector3.Distance(b.position, d.position) < 10 || Vector3.Distance(c.position, d.position) <= 20)
+            {
+                if (Vector3.Distance(a.position, b.position) >= 10 || Vector3.Distance(a.position, c.position) >= 10 || Vector3.Distance(a.position, d.position) < 10 || Vector3.Distance(b.position, c.position) < 10 || Vector3.Distance(b.position, d.position) >= 10 || Vector3.Distance(c.position, d.position) >= 10)
+                {
+                    z = Mathf.Lerp(cam.fieldOfView, 60, Time.deltaTime * 5);
+                }
+            }
+            if (Vector3.Distance(a.position, b.position) < 10 || Vector3.Distance(a.position, c.position) < 10 || Vector3.Distance(a.position, d.position) < 10 || Vector3.Distance(b.position, c.position) < 10 || Vector3.Distance(b.position, d.position) < 10 || Vector3.Distance(c.position, d.position) < 10)
+            {
+                z = Mathf.Lerp(cam.fieldOfView, 30, Time.deltaTime * 5);
+            }
+        }
+        return z;
+    }
+    float ZoomDistancia(List<GameObject> m)
+    {
+        float z = 0;
+
+        return z;
     }
 
 }
