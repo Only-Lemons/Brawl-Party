7a7068235b4c8fa4e572e8a6396d827c6f5c31f7
Weverton <wevertonmbw@gmail.com>
2019-09-11 16:49:30 -0300

    fiz instanciar bonitinhh oshoe

--- TCC/Assets/_Prototype/Scripts/Camera/TesteInstancias.cs
+++ TCC/Assets/_Prototype/Scripts/Camera/TesteInstancias.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class TesteInstancias : MonoBehaviour
 {
     public GameObject[] go;
+<<<<<<< Updated upstream
     public int velocidadeQueda = 10;
+=======
+    public GameObject cxPadrao;
+    public int velocidadeQueda = 3;
+>>>>>>> Stashed changes
 
     GameObject goAtual;
     Vector3 pontoRef;
@@ -34,8 +39,14 @@ public class TesteInstancias : MonoBehaviour
 
         if (goAtual == null)
         {
-            goAtual = Instantiate(go[Random.Range(0, go.Length)], pontoRef, Quaternion.identity);
+            //goAtual = Instantiate(go[Random.Range(0, go.Length)], pontoRef, Quaternion.identity);
+            goAtual = Instantiate(cxPadrao, pontoRef, Quaternion.identity);
         }
+    }
+
+    void InstaciarCaixa()
+    {
+
     }
 
     void Instanciar()
@@ -48,6 +59,36 @@ public class TesteInstancias : MonoBehaviour
         }
     }
 
+    public GameObject InstanciarArma(int b)
+   {
+       int rnd = Random.Range(0,5);
+       switch(rnd)
+       {
+            case 0:
+                go[b].GetComponent<ArmaController>().actualArma = new Pistol() ;
+                break;
+            case 1:
+             go[b].GetComponent<ArmaController>().actualArma = new Pistol();
+                break;
+  
+            case 2:
+             go[b].GetComponent<ArmaController>().actualArma = new Shotgun();
+                break;
+  
+            case 3:
+             go[b].GetComponent<ArmaController>().actualArma = new Shotgun();
+                break;
+  
+            case 4:
+             go[b].GetComponent<ArmaController>().actualArma = new Pistol();
+                break;
+
+            default:
+                break;
+       }
+       return go[b];
+   }
+
     void ControlarQueda()
     {
         if(goAtual != null)
@@ -56,9 +97,18 @@ public class TesteInstancias : MonoBehaviour
             if (goAtual.transform.position.y > 0.5)
             {
                 goAtual.transform.position = new Vector3(valor.x, valor.y -= Time.deltaTime*velocidadeQueda, valor.z);
+                goAtual.transform.Rotate(0,Mathf.PingPong(Time.time, 2)-1,0);
+                
             }
             else
             {
+                int b = Random.Range(0, go.Length);
+                if(b != 3)
+                    Instantiate(go[b], goAtual.transform.position, Quaternion.identity);
+                else
+                    Instantiate(InstanciarArma(b), goAtual.transform.position, Quaternion.identity);
+                    
+                Destroy(goAtual);
                 goAtual = null;
             }
         }
