b8494cdf7af8063109079a6f30c0b7f5344779e6
samueldk12 <samuel.arao@gmail.com>
2019-09-13 12:20:26 -0300

    Ajuste em PU, Add Passiva Furry

---
+++ TCC/Assets/_Prototype/Scripts/SO's/Passivas/SOPassiveFurry.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SOPassiveFurry : SOPassive
+{
+    Arma armaAtual;
+    bool isActive = false;
+    float time = 2f;
+    public override void AtivarPassiva(PlayerController player)
+    {
+        if (Cronometro())
+            FinishPassive(player);
+        if (player.actualArma != null && isActive == false)
+        {
+            if (armaAtual != player.actualArma)
+            {
+                armaAtual = player.actualArma;
+                player.speed += player.player.speed;
+                isActive = true;
+            }
+
+        }
+        if(player.PowerUp && isActive == false)
+        {
+            armaAtual = player.actualArma;
+            player.speed += player.player.speed;
+            isActive = true;
+        }
+
+    }
+    void FinishPassive(PlayerController player)
+    {
+        isActive = false;
+        player.speed -= player.player.speed;
+    }
+    bool Cronometro()
+    {
+        if (isActive == true)
+        {
+            time -= Time.deltaTime;
+            if (time <= 0)
+            {
+                time = 2f;
+                return true;
+            }            
+              
+            
+        }
+        return false;
+    }
+
+}
