34c6ba7f248726eba4b2739c7785b3e20e94544d
samueldk12 <samuel.arao@gmail.com>
2019-09-21 23:01:38 -0300

    sla

--- TCC/Assets/_Prototype/Scripts/Player/PlayerController.cs
+++ TCC/Assets/_Prototype/Scripts/Player/PlayerController.cs
@@ -74,6 +74,7 @@ public class PlayerController : MonoBehaviour, Inputs.IPlayerActions
         canDeath = true;
         UIManager.instance.onChangeValues += uiUpdate;
         UIManager.instance.onStartValues += uiStart;
+
     }
 
     void OnEnable()
