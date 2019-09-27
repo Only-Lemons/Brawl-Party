14446ec4358ed7a1e83811c3288fb2ddd6ae9a81
samueldk12 <samuel.arao@gmail.com>
2019-09-27 13:49:09 -0300

    Criação cena

---
+++ TCC/Assets/_Prototype/Scripts/GameModes/Dominion.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dominion : IGameMode
+{
+    public void DeathRule(PlayerController player)
+    {
+      
+    }
+
+    public void FinishGame()
+    {
+       
+    }
+
+    public void PointRule(PlayerController player)
+    {
+     
+    }
+
+    public void StartGame()
+    {
+
+    }
+
+    public void WinRule()
+    {
+
+    }
+   
+}
--- TCC/Assets/_Prototype/Scripts/Manager/GameManager.cs
+++ TCC/Assets/_Prototype/Scripts/Manager/GameManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    private GameController sceneController;
+    public GameObject playerInputPrefab;
+    public List<GameObject> playerInputsPrefabs;
     private void Awake()
     {
         Instance = this;
