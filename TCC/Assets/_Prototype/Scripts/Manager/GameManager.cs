2cf25ec3905e5f5899df9e8648d4ee16d43ef7ea
samueldk12 <samuel.arao@gmail.com>
2019-10-01 13:45:43 -0300

    Revert "Merge branch 'master' of https://github.com/ppsavila/ArenaTCC"
    This reverts commit 51633b1f8e89125c627a975b5f8e01748193737a, reversing
    changes made to 318fb61f54adf1f4b9f56dc90cd720d35af94c47.

--- TCC/Assets/_Prototype/Scripts/Manager/GameManager.cs
+++ TCC/Assets/_Prototype/Scripts/Manager/GameManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,13 +9,14 @@ public class GameManager : MonoBehaviour
     public GameObject playerInputPrefab;
     public IGameMode gameMode;
     public GameController gameController;
+    public List<GameObject> playerInputsPrefabs;
     public float TimeInGame = 120;
     public GameModes newGameMode;
     public List<PlayerSelect> playersPanels = new List<PlayerSelect>();
 
     #region LevelInteract
     public int nextLevel;
-    #endregion  
+    #endregion
     private void Awake()
     {
         if (Instance != null)
@@ -66,11 +66,6 @@ public class GameManager : MonoBehaviour
             gameController.gameMode = new FreeForAll(gameController, TimeInGame);
         }
     }
-
-    private void Update()
-    {
-
-    }
-
+    
 
 }
