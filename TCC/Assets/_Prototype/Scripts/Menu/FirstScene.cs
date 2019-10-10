68756992b29c8c96ac74b1899a9ffb5ba9e5fe4e
wevertonmbw <wevertonmbw@gmail.com>
2019-10-10 02:53:05 -0300

    Refino tela 'press start'

--- TCC/Assets/_Prototype/Scripts/Menu/FirstScene.cs
+++ TCC/Assets/_Prototype/Scripts/Menu/FirstScene.cs
@@ -8,7 +8,8 @@ using System;
 public class FirstScene : MonoBehaviour
 {
     public Text textPress;
-    float actualTime = 1;
+    float actualTime = 1.8f;
+    float timeRun = 0;
     void Update()
     {
         if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
@@ -17,10 +18,29 @@ public class FirstScene : MonoBehaviour
         }
 
         CrossTextColor();
+        PiscarLetreiro();
     }
 
     private void CrossTextColor()
     {
-       
+        
+    }
+
+    void PiscarLetreiro()
+    {
+        timeRun += Time.deltaTime;
+        if (timeRun >= actualTime)
+        {
+            StopAllCoroutines();
+            StartCoroutine(IEPiscarLetreiro());
+            timeRun = 0;
+        }
+    }
+
+    IEnumerator IEPiscarLetreiro()
+    {
+        textPress.gameObject.SetActive(false);
+        yield return new WaitForSeconds(actualTime/2);
+        textPress.gameObject.SetActive(true);
     }
 }
