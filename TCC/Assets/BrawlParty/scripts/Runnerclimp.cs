e2ccad73e8ffc3c566c82ad4ca06646e50271ef2
wevertonmbw <51509839+wevertonmbw@users.noreply.github.com>
2020-03-09 15:01:50 -0300

    #213

---
+++ TCC/Assets/BrawlParty/scripts/Runnerclimp.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runnerclimp : MiniGame
+{
+    public List<PlayerController> players = new List<PlayerController>();
+
+    void Start()
+    {
+        players = new List<PlayerController>(FindObjectsOfType<PlayerController>());
+
+        foreach (var player in players)
+        {
+            player.actualGameMode = this;
+        }
+    }
+
+    public override void Action(PlayerController player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void HitRule(PlayerController player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void MovementRule(PlayerController player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void PointRule(PlayerController player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void RotationRule(PlayerController player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void WinRule()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
