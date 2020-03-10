7f9d04e3dd1a76bda10c56ef486b48a1a1bbfc8e
Weverton Miranda <wevertonmbw@gmail.com>
2020-03-09 23:13:59 -0300

    #213-base-criada

---
+++ TCC/Assets/BrawlParty/scripts/Platform.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(this.gameObject, 5);
+    }
+
+}
--- TCC/Assets/BrawlParty/scripts/Runnerclimp.cs
+++ TCC/Assets/BrawlParty/scripts/Runnerclimp.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class Runnerclimp : MiniGame
 {
     public List<PlayerController> players = new List<PlayerController>();
+    public Camera camera;
+    public float timeUp;
+    public int speed;
+
+    public GameObject[] platform;
+    public Transform[] posPlatform;
+    float timePlatform;
+    public float timeInstantiatePlatform;
+
 
     void Start()
     {
@@ -14,42 +23,67 @@ public class Runnerclimp : MiniGame
         {
             player.actualGameMode = this;
         }
+
+        camera = Camera.main;
     }
 
     public override void Action(PlayerController player)
     {
-        throw new System.NotImplementedException();
     }
 
     public override void HitRule(PlayerController player)
     {
-        throw new System.NotImplementedException();
     }
 
     public override void MovementRule(PlayerController player)
     {
-        throw new System.NotImplementedException();
+        player.transform.position += player._movementAxis * player.speed * Time.fixedDeltaTime;
     }
 
     public override void PointRule(PlayerController player)
     {
-        throw new System.NotImplementedException();
     }
 
     public override void RotationRule(PlayerController player)
     {
-        throw new System.NotImplementedException();
     }
 
     public override void WinRule()
     {
-        throw new System.NotImplementedException();
+        
+    }
+
+    void SceneMechanics() //Comportamento geral do cenário
+    {
+        timeUp += Time.fixedDeltaTime / speed;
+        if(timeInstantiatePlatform > 0.3f)
+            timeInstantiatePlatform -= timeUp / 2000;
+        float time = Time.fixedDeltaTime * Acceleration(timeUp);
+        camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y + time, camera.transform.position.z);
+
+        PlatformGenerator();
+    }
+    float Acceleration(float t) //Aumenta dificuldade do jogo gradualmente
+    {
+        return Mathf.Exp(t);
+    }
+
+    void PlatformGenerator() //Comportamento e geração das plataformas
+    {
+        timePlatform += Time.deltaTime;
+        if(timePlatform > timeInstantiatePlatform)
+        {
+            GameObject plat = Instantiate(platform[Random.Range(0, platform.Length)], posPlatform[Random.Range(0, posPlatform.Length)].position, Quaternion.identity);
+
+            timePlatform = 0;
+        }
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        
+        SceneMechanics();
+        WinRule();
     }
 }
