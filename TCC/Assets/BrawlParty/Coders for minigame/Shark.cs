1ac3cd3bb60abdacb00a9726664ae3309fefcce3
samueldk12 <samuel.arao@gmail.com>
2020-03-10 23:19:52 -0300

    Sharkmakethis

---
+++ TCC/Assets/BrawlParty/Coders for minigame/Shark.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shark : MonoBehaviour
+{
+    Vector3 nextFence;
+    bool inJump = false;
+    private void Update()
+    {
+        if (!inJump)
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, nextFence, 1);
+        }
+    }
+    public void Moviment(Vector3 postion)
+    {
+        nextFence = new Vector3(postion.x, postion.y-2, postion.z);
+    }
+    public void Jump()
+    {
+        if (!inJump)
+        {
+           // this.transform.position = Vector3
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Destroied")
+            GameObject.Destroy(other.gameObject);
+        PlayerController aux = other.GetComponent<PlayerController>();
+        if (aux != null)
+        {
+            aux.actualGameMode.HitRule(aux);
+        }
+            
+    }
+
+}
--- TCC/Assets/BrawlParty/scripts/SharkEater.cs
+++ TCC/Assets/BrawlParty/scripts/SharkEater.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class SharkEater : MiniGame
 {
-    public List<PlayerController> players = new List<PlayerController>();
+    [SerializeField]
+    List<PlayerController> players = new List<PlayerController>();
+    [SerializeField]
+    List<Shark> sharks = new List<Shark>();
+    [SerializeField]
+    List<GameObject> fences = new List<GameObject>();
+    Dictionary<PlayerController, float> blooding = new Dictionary<PlayerController, float>();
+    Dictionary<PlayerController, float> timeInBlooding = new Dictionary<PlayerController, float>();
+    Dictionary<PlayerController, Shark> sharkPerPlayer = new Dictionary<PlayerController, Shark>();
+    float timeToEat = 0;
+    bool playerWin;
 
     void Start()
     {
+        playerWin = false;
         players = new List<PlayerController>(FindObjectsOfType<PlayerController>());
 
         foreach (var player in players)
@@ -15,40 +26,111 @@ public class SharkEater : MiniGame
             player.actualGameMode = this;
         }
     }
+    void stopBlooding()
+    {
+        foreach (var player in players)
+        {
+            if(timeInBlooding[player] > 0)
+            {
+                timeInBlooding[player] -= Time.deltaTime;
+                if (timeInBlooding[player] <= 0) {
+                    timeInBlooding[player] = 0;
+                    blooding[player] = 0;
+                }
+            }
+        }
+    }
     public override void Action(PlayerController player)
     {
-        throw new System.NotImplementedException();
+        blooding[player] += 0.1f;
+        timeInBlooding[player] += 3f;
     }
-
     public override void HitRule(PlayerController player)
     {
-        throw new System.NotImplementedException();
+        players.Remove(player);
+        WinRule();
+        if (!playerWin) {
+            GameObject.Destroy(player.gameObject);
+        }
     }
 
     public override void MovementRule(PlayerController player)
     {
-        throw new System.NotImplementedException();
+        // COPIAR DO PEDRO  
+        player.transform.position += player._movementAxis * player.speed * Time.fixedDeltaTime;
     }
 
     public override void PointRule(PlayerController player)
     {
-        throw new System.NotImplementedException();
+        
     }
 
     public override void RotationRule(PlayerController player)
     {
-        throw new System.NotImplementedException();
+        // COPIAR DO PEDRO
     }
 
     public override void WinRule()
     {
-        throw new System.NotImplementedException();
+        if (players.Count == 0)
+        {
+            playerWin = true;
+            //empate
+
+
+        } else if (players.Count == 1)
+        {
+            playerWin = true;
+            //um ganhou sozinho
+        }
     }
+    Vector3 getNearestFence(PlayerController player)
+    {
+        Vector3 aux = fences[0].transform.position;
+        float minDistance = Vector3.Distance(player.transform.position, fences[0].transform.position);
+        foreach (var fence in fences)
+        {
+            if(Vector3.Distance(player.transform.position,fence.transform.position)< minDistance)
+            {
+                aux = fence.transform.position;
+                minDistance = Vector3.Distance(player.transform.position, fence.transform.position);
+            }
+        }
+        return aux;
+    }
+    void sharkController()
+    {
+        timeToEat += Time.deltaTime;
+        if (timeToEat > 1)
+        {
+            
+            foreach (var shark in sharks)
+            {
+                Vector3 aux = Vector3.zero;
+                foreach (var player in players)
+                {
+                    if(Random.Range(0f,1f) < blooding[player])
+                    {
+                        aux = getNearestFence(player);
+                    }
+                }
+                if (aux == Vector3.zero) {
+                    int numCer = Random.Range(0, fences.Count);
+                    aux = fences[numCer].transform.position;
+                }
+                shark.Moviment(aux);
+                if(timeToEat >= 3)
+                {
+                    timeToEat = 0;
+                    shark.Jump();
+                }
 
-
-    // Update is called once per frame
+            }
+        }
+    }
     void Update()
     {
-        
+        sharkController();
+        stopBlooding();
     }
 }
