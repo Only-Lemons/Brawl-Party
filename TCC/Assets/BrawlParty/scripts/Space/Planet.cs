e847d09318114c0c23ec286f237d53afece9dd99
Pedro Paulo <ppsavila2@gmail.com>
2020-03-11 16:24:57 -0300

    mudança

--- TCC/Assets/BrawlParty/scripts/Space/Planet.cs
+++ TCC/Assets/BrawlParty/scripts/Space/Planet.cs
@@ -4,35 +4,34 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    public float gravityScale = 12f, planetRadius = 2f, gravityMinRange = 1f, gravityMaxRange = 2f;
+    public float gravityScale, planetRadius, gravityMinRange, gravityMaxRange;
     public GameObject planet, minRS, maxRS;
 
-    public Transform pos;
+    public Transform gravityPoint;
 
+    private void Start()
+    {
+        gravityPoint = this.transform;
+    }
 
 
     void OnTriggerStay(Collider obj)
     {
-        //    float gravitationalPower = gravityScale / planetRadius;
-        //    float dist = Vector3.Distance(obj.transform.position, transform.position);
+        float gravitationalPower = gravityScale / planetRadius;
 
-        //    //if (dist > (planetRadius + gravityMinRange))
-        //    //{
-        //    //    float min = planetRadius + gravityMinRange + 0.5f;
-        //    //    gravitationalPower = gravitationalPower * (((min + gravityMaxRange) - dist) / gravityMaxRange);
-        //    //}
+        //float dist = Vector3.Distance(obj.transform.position, transform.position);
+        //if (dist > (planetRadius + gravityMinRange))
+        //{
+        //    float min = planetRadius + gravityMinRange + 0.5f;
+        //    gravitationalPower = gravitationalPower * (((min + gravityMaxRange) - dist) / gravityMaxRange);
+        //}
 
-        //    Vector3 dir = (transform.position - obj.transform.position) * gravityScale;
-        //    obj.GetComponent<Rigidbody>().AddForce(dir);
+        Vector3 dir = (transform.position - obj.transform.position) * gravityScale;
+        obj.GetComponent<Rigidbody>().AddForce(dir);
 
-        //    if (obj.CompareTag("Player"))
-        //    {
-        //        obj.transform.up = Vector3.MoveTowards(obj.transform.up, -dir, gravitationalPower * Time.deltaTime * 5f);
-        //    }
-
-        
+        if (obj.CompareTag("Player"))
+        {
+            obj.transform.up += Vector3.MoveTowards(obj.transform.up, -dir, gravityScale * Time.deltaTime * 5f);
+        }
     }
-
-
-
 }
