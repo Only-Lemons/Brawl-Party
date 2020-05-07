a1959ccc86dd83bb7ea6a34159409081b7759a51
Michael Oliveira <michael.oliveria@fanatee.com>
2020-05-06 23:34:15 -0300

    #0505 - Fix Some Problems on Prefabs

---
+++ TCC/Assets/BrawlParty/prefabs/VFX/crossRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crossRotate : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject go;
+
+    void Update()
+    {
+        go.transform.Rotate(0, 2, 0);
+    }
+}
