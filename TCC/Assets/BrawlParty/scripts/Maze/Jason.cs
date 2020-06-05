3754a8844a06b328ef7f8ed3ac6ae863dbcac4d8
wevertonmbw <wevertonmbw@gmail.com>
2020-06-05 18:40:21 -0300

    maze arrumado

--- TCC/Assets/BrawlParty/scripts/Maze/Jason.cs
+++ TCC/Assets/BrawlParty/scripts/Maze/Jason.cs
@@ -6,9 +6,11 @@ public class Jason: MonoBehaviour
 {
     NavMeshAgent IAmoviment;
     PlayerController target;
+    Maze maze;
        
     void Start()
     {
+        maze = GameObject.FindObjectOfType<Maze>();
         IAmoviment = GetComponent<NavMeshAgent>();
     }
 
@@ -18,6 +20,8 @@ public class Jason: MonoBehaviour
         PlayerController player = other.GetComponent<PlayerController>();
         if(player != null)
         {
+            int pos = Random.Range(0, maze.keysSpawn.Length);
+            transform.position = maze.keysSpawn[pos].transform.position;
             player.actualGameMode.HitRule(player);
         }
     }
